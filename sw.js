a1cfbf80476a9724480158d0268436184ba90cff
ابا مالك العقيلي <abamalkalqyly66@gmail.com>
2026-04-13 11:12:29 +0200

    Add new assets to service worker cache

--- sw.js
+++ sw.js
@@ -1,23 +1,24 @@
 const cacheName = 'aba-malik-v1';
+// أضفنا هنا أسماء ملفاتك الجديدة
 const assets = [
   './',
   './index.html',
+  './install.js',
   './manifest.json',
   'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
   'https://i.ibb.co/pBhzxHdM/1000027317.jpg'
 ];
 
-// تثبيت ملفات الموقع في ذاكرة الهاتف
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
     })
   );
 });
 
-// عرض الملفات من الذاكرة عند انقطاع الإنترنت
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => {
@@ -25,3 +26,4 @@ self.addEventListener('fetch', e => {
     })
   );
 });
+    

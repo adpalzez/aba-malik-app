81e5c49cc44540782e0b949873308304df0de676
ابا مالك العقيلي <abamalkalqyly66@gmail.com>
2026-04-13 10:16:09 +0200

    Implement service worker for asset caching
    This service worker caches specified assets for offline use and serves them when the network is unavailable.

---
+++ sw.js
@@ -0,0 +1,27 @@
+const cacheName = 'aba-malik-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://i.ibb.co/pBhzxHdM/1000027317.jpg'
+];
+
+// تثبيت ملفات الموقع في ذاكرة الهاتف
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// عرض الملفات من الذاكرة عند انقطاع الإنترنت
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
